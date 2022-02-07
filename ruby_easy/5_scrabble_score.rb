64ffa61027655fd72c10e0eeed70a1016de3e3cd
mskalisz <mskalisz3@gmail.com>
2022-02-06 21:09:33 -0500

    Add LS solution for scrabble score

--- ruby_easy/5_scrabble_score.rb
+++ ruby_easy/5_scrabble_score.rb
@@ -113,3 +113,104 @@ class Scrabble
     Scrabble.new(string).score
   end
 end
+
+=begin
+# Problem
+For our program, the details we need to keep in mind are as follows:
+
+Our input will be a word for which we need to calculate the Scrabble score.
+The numeric value for letters is already determined for us:
+A, E, I, O, U, L, N, R, S, T — 1 point
+D, G — 2 points
+B, C, M, P. — 3 points
+F, H, V, W, Y — 4 points
+K — 5 points
+J, X — 8 points
+Q, Z — 10 points
+Each letter, even if repeated, counts towards the total.
+
+# Examples
+From the test cases shown above, we can see that we need to create a Scrabble class. The class should have 
+3 methods as follows:
+
+A constructor that accepts a Scrabble word as its argument.
+
+A score method that calculates the score of the Scrabble word defined by the constructor. score takes no 
+arguments and returns the score for the word. From the test cases, we can see that:
+
+Invalid words should return a score of 0.
+Words are case-insensitive (abc and ABC represent the same word).
+A class or static method also named score. Class and static methods are not executed by instances of the 
+main class, but by the class itself. This version of score takes a single argument that contains the 
+desired Scrabble word and returns its score.
+
+We may also want to create some helper methods to assist us, but those are optional.
+
+# Data
+Our input is usually a string, but we also need to deal with invalid input. It may be helpful to also use 
+an array to calculate the score, and an object to keep track of our scoring standard.
+
+# Hints
+We need two methods named score: one is an instance method, the other is a class or static method.
+
+We should deal with invalid letters in the score instance method.
+The score class/static method can take advantage of the constructor and the score instance method.
+
+# Algo
+constructor
+Accept a Scrabble word as an argument and save it for later use.
+Ignore bad input at this point.
+
+Method: score
+Return 0 if word is invalid.
+Iterate through each letter and increment score by value of each letter.
+Return the final score.
+
+Method: score (static or class method)
+Accept a Scrabble word as an argument.
+We can leverage the constructor and score method described above.
+Return the score.
+
+# Code & Discussion
+
+class Scrabble
+  attr_reader :word
+
+  POINTS = {
+    'AEIOULNRST'=> 1,
+    'DG' => 2,
+    'BCMP' => 3,
+    'FHVWY' => 4,
+    'K' => 5,
+    'JX' => 8,
+    'QZ' => 10
+}
+  def initialize(word)
+    @word = word ? word : ''
+  end
+
+  def score
+    letters = word.upcase.gsub(/[^A-Z]/, '').chars
+
+    total = 0
+    letters.each do |letter|
+      POINTS.each do |all_letters, point|
+        total += point if all_letters.include?(letter)
+      end
+    end
+    total
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+  end
+end
+
+In our constructor, we assign @word to the given argument as long as it's not nil. Within score, we clean up 
+the string to ensure we're only dealing with letters. Then, we iterate over each letter, as well as each 
+key-value pair in our POINTS hash, and add the appropriate points to our total. Notice, this allows for 
+empty strings to work fine because they essentially skip the iterations and retain the score of 0 from the 
+initialization of total.
+
+For our score class method, we borrow all of the functionality of its instance method counterpart.
+=end

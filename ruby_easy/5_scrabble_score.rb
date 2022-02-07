f3297449cab616f18fe9a0d21c5997eb89219e8e
mskalisz <mskalisz3@gmail.com>
2022-02-06 20:46:22 -0500

    Solve scrabble score

--- ruby_easy/5_scrabble_score.rb
+++ ruby_easy/5_scrabble_score.rb
@@ -32,4 +32,78 @@ Thus, to compute the final total (14 points), we count:
 => 5 + 9
 => 14
 
+# Problem
+Given a word, calculate the point value
+
+# Example
+Scrabble.new('street').score == 6
+
+Scrabble.new('quirky').score == 22
+
+Scrabble.new('OXYPHENBUTAZONE').score == 41
+
+# Data
+String, array, hash for the letter to point conversion
+
+# Algo
+
+Initialize a constant with alphabet and point values
+
+Constructor
+- Accepts a string
+
+Score
+- Initiate a results variable
+- Split the word into an array
+- Iterate through the array
+  - Upcase each, find point value
+  - Add point value to result
+- Return result
+
 =end
+
+class Scrabble
+  attr_reader :word
+
+  SCORES = {
+            "A" => 1, 
+            "E" => 1, 
+            "I" => 1,
+            "O" => 1, 
+            "U" => 1, 
+            "L" => 1, 
+            "N" => 1, 
+            "R" => 1, 
+            "S" => 1, 
+            "T" => 1,
+            "D" => 2, 
+            "G"	=> 2,
+            "B" => 3, 
+            "C" => 3, 
+            "M" => 3, 
+            "P"	=> 3,
+            "F" => 4, 
+            "H" => 4, 
+            "V" => 4, 
+            "W" => 4, 
+            "Y"	=> 4,
+            "K"	=> 5,
+            "J" => 8, 
+            "X"	=> 8,
+            "Q" => 10, 
+            "Z"	=> 10,
+          }
+
+  def initialize(string)
+    @word = string
+  end
+
+  def score
+    results = 0
+    word.chars.each do |letter|
+      letter = letter.upcase
+      results += SCORES[letter]
+    end
+    results
+  end
+end

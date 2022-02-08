145e1632c1f60b00e702340c48d0573cab265eab
mskalisz <mskalisz3@gmail.com>
2022-02-07 21:10:11 -0500

    Add LS solution and PEDAC for perfect number challenge

--- ruby_easy/6_perfect_number.rb
+++ ruby_easy/6_perfect_number.rb
@@ -72,3 +72,112 @@ class PerfectNumber
     raise StandardError.new("Not a valid number.") unless num > 0
   end
 end
+
+=begin
+LS Solution
+
+# Problem
+For our program, the details we need to keep in mind are as follows:
+
+We'll be given a number and we need to determine whether it is abundant, perfect, or deficient.
+Perfect: Sum of factors = number
+Abundant: Sum of factors > number
+Deficient: Sum of factors < number
+Factors are the numbers less than the input number that can be evenly divided into it. For example, the 
+number 6 can be evenly divided by 1, 2, and 3.
+
+# Examples
+From the test cases shown above, we can see that we need to create a PerfectNumber class. The class should 
+have 1 method:
+
+It accepts a number as an argument and returns the appropriate string based on its classification: 
+"perfect", "abundant", or "deficient".
+We can also see that we need to throw an error when the input to the above method is a negative number.
+
+We may also want to create some helper methods to assist us, but those are optional.
+
+# Data 
+In addition to working with numbers, it may also be helpful to use a collection that can hold a range. 
+This could prove useful in finding all of the divisors of a given number.
+
+# Hints
+Throw an error if the input is negative
+How can we efficiently find all of the factors of the given number?
+We also need to sum all of the factors.
+Once we have the sum, we can classify the number and return the result.
+A number is a divisor of another number if the remainder of dividing the second number by the first is 0. 
+For example, 7 is a divisor of 21 since 21 % 7 is 0. However, 7 is not a divisor of 23 since 23 % 7 is not 
+0 -- it is 2.
+
+# Algo
+Method: classify
+
+Throw error if the argument is less than 1.
+Find the sum of all factors of argument (helper method)
+If sum is equal to argument, return "perfect".
+If sum is greater than argument, return "abundant".
+If sum is less than argument, return "deficient".
+Helper Method: determine sum of factors
+
+Initialize sum to 0
+Iterate over numbers from 1 up to 1 less than the argument.
+If number is a divisor of the argument, add number to sum
+Return sum
+
+# Code
+
+class PerfectNumber
+  def self.classify(number)
+    raise StandardError.new if number < 1
+    sum = sum_of_factors(number)
+
+    if sum == number
+      'perfect'
+    elsif sum > number
+      'abundant'
+    else
+      'deficient'
+    end
+  end
+
+  class << self
+    private
+
+    def sum_of_factors(number)
+      (1...number).select do |possible_divisor|
+        number % possible_divisor == 0
+      end.sum
+    end
+  end
+end
+
+In our example cases, we could see that we needed to create a class method, so no constructor was needed. 
+First, we make sure to raise an error if the input is negative. Then, we use our helper method 
+sum_of_factors to calculate the sum of all the divisors.
+
+Within the helper method, we iterate over 1 up to one less than the given number. We select numbers from 
+this range that are even divisors using the modulo, and then we add all of the selected numbers using sum.
+
+Note that our sum_of_factors helper method is a class (or static) method. We'd like the method to be 
+private since it's of no use to the users of our class. Unfortunately, this code won't work as expected:
+
+class PerfectNumber
+  # omitted code
+
+  private
+
+  def self.sum_of_factors(number)
+    (1...number).select do |possible_divisor|
+      number % possible_divisor == 0
+    end.sum
+  end
+end
+
+Even though it looks like we're creating a private method, the private method doesn't work on class 
+methods -- it only works on instance methods. Fortunately, there is a way around that using the 
+class << self idiom lets us operate on the class itself as an object; by adding a private instance 
+method to the class object, we effectively create a private class method. Why this works isn't important 
+at this stage, but it's a useful idiom to know.
+
+Back in classify, we use this sum to determine the classification of the number.
+=end

1ce9b4aa05475093d9f071f31e22e2054cca8b6c
mskalisz <mskalisz3@gmail.com>
2022-02-10 11:00:55 -0500

    Add PEDAC for beer song

--- ruby_easy/9_beer_song.rb
+++ ruby_easy/9_beer_song.rb
@@ -3,4 +3,31 @@ Beer Song
 
 Write a program that can generate the lyrics of the 99 Bottles of Beer song. 
 See the test suite for the entire song.
+
+
+# Problem
+- Create a program which generates the lyrics to 99 bottles of beer
+Expected methods: 
+  - lyrics: outputs entire song 99 to 0
+  - verse: takes *parameters, outputs those verses
+    - Special verses: 1, 0
+
+# Example
+test suite
+
+# Data
+- String, integer, range?, array
+
+# Algo
+constructor
+ - none necessary
+
+lyrics
+ - output each verse from 99 to 0
+
+verse
+ - accept a parameter
+ - outputs the lyrics with the correct countdown
+ - special for 0 and 1
 =end
+
